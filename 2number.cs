b7f903756f046990c6e1720fd9ce257ff3b1b686
vasiliy <vasya-timkin@mail.ru>
2023-03-16 18:18:18 +0300

    2number

---
+++ 2number.cs
@@ -0,0 +1,5 @@
+﻿//Задача №10
+Console.Write("Введи трёхзначное число: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+Console.WriteLine(stringNumber[1]);
